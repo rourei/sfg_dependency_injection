63fbadb8f9924783e6a53876d3cfe6c7720c9549
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-14 19:02:00 +0200

    [L39] added default Profile

--- src/main/java/guru/springframework/sfg_dependency_injection/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfg_dependency_injection/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfg_dependency_injection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES") // specify Profile Qualifier
+@Profile({"ES", "default"}) // specify Profile Qualifier
 @Service("i18nService") // give this the same Qualifier as the English version
 public class I18nSpanishGreetingService implements GreetingService {
 
